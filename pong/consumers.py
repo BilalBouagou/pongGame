185d977817901c82a557b33c85754bf19f6a0b1f
Bilal Bouagou <bbouagou@c1r3p7.1337.ma>
2024-11-02 10:57:00 +0100

    bruh

--- pong/consumers.py
+++ pong/consumers.py
@@ -2,30 +2,55 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class GameConsumer(AsyncWebsocketConsumer):
-	playersNum = 0
+    players = set()
+    playersNum = 0
 
-	@classmethod
-	async def assignPlayer(self):
-		self.playersNum += 1
-		if (self.playersNum <= 2):
-			return f'player {self.playersNum}'
-		else:
-			return 'spectator'
+    async def assignPlayer(self):
+        self.playersNum += 1
+        if self.playersNum <= 2:
+            player_id = f'player {self.playersNum}'
+            self.players.add(player_id)
+            return player_id
+        else:
+            return 'spectator'
 
-	async def connect(self):
-		await self.accept()
-		playerId = self.assignPlayer()
-		status = 'connected' if self.playersNum == 2 else 'waiting'
-		await self.send(text_data=json.dumps({
-			'assignment' : playerId,
-			'status' : status
-		}))
+    async def connect(self):
+        await self.accept()
+        playerId = await self.assignPlayer()
+        status = 'connected' if self.playersNum == 2 else 'waiting'
+        await self.send(text_data=json.dumps({
+            'assignment': playerId,
+            'status': status
+        }))
+        if (self.playersNum == 2):
+        	await self.broadcast_player_count()
 
-	async def disconnect(self, close_code):
-		pass
+    async def disconnect(self, close_code):
+        if self.playersNum > 0:
+            self.players.remove(f'player {self.playersNum}')
+            self.playersNum -= 1
+            await self.broadcast_player_count()
 
-	async def receive(self, text_data):
-		data = json.loads(text_data)
-		await self.send(text_data=json.dumps({
-			'message': 'Your response here'
-		}))
+    async def broadcast_player_count(self):
+        count = len(self.players)
+        for player in self.players:
+            await self.channel_layer.group_send(
+                player,
+                {
+                    'type': 'player_count',
+                    'count': count
+                }
+            )
+
+    async def player_count(self, event):
+        count = event['count']
+        await self.send(text_data=json.dumps({
+            'type': 'player_count',
+            'count': count
+        }))
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.send(text_data=json.dumps({
+            'message': 'Your response here'
+        }))
